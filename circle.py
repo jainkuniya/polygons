108588d1334245b1f1f5dfcb165c0c4ea17f1270
jainkuniya <vishwesh.jainkuniya@aux.go-jek.com>
2019-04-29 02:02:39 +0530

    cicle: Create method to find circumference.

--- circle.py
+++ circle.py
@@ -2,3 +2,6 @@ def find_area(r):
     PI = 3.142
     return PI * (r*r); 
  
+def find_circumference(r): 
+    PI = 3.1415
+    return (2 * PI * r) 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from circle import find_area
+from circle import find_area, find_circumference
 
 r = 5
 area = find_area(5)
+circumference = find_circumference(5)
 
 print(f'Area of cicle with radius {r} is {area}')
+print(f'Circumference of cicle with radius {r} is {circumference}')
