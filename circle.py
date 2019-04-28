0c677343fb4053472456ddfcf5f90dbd14fb2fca
jainkuniya <vishwesh.jainkuniya@aux.go-jek.com>
2019-04-29 01:58:52 +0530

    cicle: Create method to find area.

---
+++ circle.py
@@ -0,0 +1,4 @@
+def find_area(r): 
+    PI = 3.142
+    return PI * (r*r); 
+ 
---
+++ main.py
@@ -0,0 +1,6 @@
+from circle import find_area
+
+r = 5
+area = find_area(5)
+
+print(f'Area of cicle with radius {r} is {area}')
